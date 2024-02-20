153deb4ba94fbb1597c49ad9d16ee593236eade4
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2024-02-20 16:37:12 -0300

    questão 6 da lista1 extra

---
+++ codigos-java/src/ControleDeAudio.java
@@ -0,0 +1,54 @@
+public class ControleDeAudio {
+    private int volume = 2;
+
+    public int aumentarVolume() {
+        if (volume < 10) {
+            return ++volume;
+        }
+        return volume;
+    }
+
+    public int diminuirVolume() {
+        if (volume != 0) {
+            return --volume;
+        }
+        return volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    //aqui na main terei só a instância do objeto volume e as chamadas dos métodos:
+    public static void main(String[] args) {
+        ControleDeAudio volume = new ControleDeAudio();
+
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+        System.out.println(volume.aumentarVolume());
+
+        System.out.println(); //só para separar
+
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+        System.out.println(volume.diminuirVolume());
+
+        System.out.println(); //só para separar
+
+        System.out.println(volume.getVolume());
+    }
+}
