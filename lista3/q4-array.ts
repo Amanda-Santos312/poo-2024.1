6410976deb1f4fd52bc231d75ae615b5964ef871
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2024-03-11 10:35:04 -0300

    questões da lista3

--- lista3/q4-array.ts
+++ lista3/q4-array.ts
@@ -2,13 +2,13 @@
 separados por traço (-) no formato string. Para isso, use o método forEach dos
 arrays.*/
 
-let numeros = [3, 12, 15, 31, 63];
+let numeros = ['3', '12', '15', '31', '63'];
 
-function printarNumeros(numeros: number[]) {
+function printarNumeros(numeros: string[]) { //aqui sendo number
     let resultado: string = "";
 
     for (let numero of numeros) {
-        resultado += numero.toString() + '-';
+        resultado += numero + '-'; //ia precisar aqui do .toString()
     }
 
     return resultado;
