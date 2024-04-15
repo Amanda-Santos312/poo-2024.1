ddfe22130cd56d2c6ff857ab5b296c35216e70b4
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2024-04-15 19:01:59 -0300

    parte da q2

---
+++ lista5/q2-microBlog.ts
@@ -0,0 +1,19 @@
+class Postagem {
+    id: number;
+    texto: string;
+    qtdCurtidas: number;
+
+    constructor(id: number, texto: string, qtdCurtidas: number) {
+        this.id = id;
+        this.texto = texto;
+        this.qtdCurtidas = qtdCurtidas;
+    }
+
+    curtir(): void {
+        this.qtdCurtidas++;
+    }
+
+    toString(): string {
+        return `Postagem: ${this.texto} \n Curtidas: ${this.qtdCurtidas}`;
+    }
+}
